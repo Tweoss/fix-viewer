de5da18d0735c3019412ed6df5350a98d2f76f92
tweoss <fchua@puffer5.stanford.edu>
2023-08-08 18:42:07 -0700

    add basic request that receives parent information

--- src/app.rs
+++ src/app.rs
@@ -1,60 +1,94 @@
+use std::sync::mpsc::{channel, Receiver, Sender};
+
+use egui::TextEdit;
+
+use crate::handle::Handle;
+
+pub struct App {
+    state: State,
+    storage: Storage,
+}
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+struct Storage {
+    url: String,
+    target: Handle,
 }
 
-impl Default for TemplateApp {
+impl Default for Storage {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            url: String::new(),
+            target: Handle::from_hex("0-0-0-2400000000000000").unwrap(),
         }
     }
 }
 
-impl TemplateApp {
+struct State {
+    target_input: String,
+    response: String,
+    error: String,
+    first_render: bool,
+    response_tx: Sender<ehttp::Result<ehttp::Response>>,
+    response_rx: Receiver<ehttp::Result<ehttp::Response>>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        let (tx, rx) = channel();
+        Self {
+            target_input: String::new(),
+            response: String::new(),
+            error: String::new(),
+            first_render: true,
+            response_tx: tx,
+            response_rx: rx,
+        }
+    }
+}
+
+impl App {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+            return Self {
+                state: State::default(),
+                storage: eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default(),
+            };
         }
 
-        Default::default()
+        App {
+            state: State::default(),
+            storage: Storage::default(),
+        }
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for App {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+        eframe::set_value(storage, eframe::APP_KEY, &self.storage);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let storage = &mut self.storage;
+        let State {
+            target_input,
+            response,
+            error,
+            first_render,
+            response_tx: tx,
+            response_rx: rx,
+        } = &mut self.state;
 
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
@@ -67,15 +101,58 @@ impl eframe::App for TemplateApp {
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Side Panel");
 
+            ui.separator();
+
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+                ui.label("URL: ");
+                TextEdit::singleline(&mut storage.url)
+                    .hint_text("127.0.0.1:9090")
+                    .desired_width(f32::INFINITY)
+                    .show(ui);
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
+            if *first_render {
+                *target_input = storage.target.to_hex();
             }
+            ui.horizontal(|ui| {
+                ui.label("Target: ");
+                if TextEdit::singleline(target_input)
+                    .desired_width(f32::INFINITY)
+                    .show(ui)
+                    .response
+                    .changed()
+                {
+                    match Handle::from_hex(&target_input) {
+                        Ok(h) => {
+                            error.clear();
+                            storage.target = h;
+                        }
+                        Err(_) => todo!(),
+                    }
+                }
+            });
+
+            if ui.button("Get Parent").clicked() {
+                let request = ehttp::Request::get(format!(
+                    "http://{}/parents?handle={}",
+                    storage.url,
+                    storage.target.to_hex(),
+                ));
+                let tx_clone = tx.clone();
+                ehttp::fetch(request, move |result: ehttp::Result<ehttp::Response>| {
+                    tx_clone.send(result).unwrap();
+                })
+            }
+
+            if let Ok(http_result) = rx.try_recv() {
+                match http_result {
+                    Ok(v) => *response = v.text().unwrap().to_string(),
+                    Err(e) => *error = e,
+                }
+            }
+
+            ui.separator();
+            ui.label(response.as_str());
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
@@ -103,6 +180,8 @@ impl eframe::App for TemplateApp {
             ));
             egui::warn_if_debug_build(ui);
         });
+
+        *first_render = false;
 
         if false {
             egui::Window::new("Window").show(ctx, |ui| {
---
+++ src/handle.rs
@@ -0,0 +1,293 @@
+use std::convert::TryFrom;
+
+use anyhow::{bail, ensure, Context, Result};
+
+/// Number of bytes
+const METADATA_LENGTH: usize = 1;
+/// 64 bit number => 8 bytes
+const UINT64_LENGTH: usize = 8;
+/// 256 bits => 32 bytes
+const HANDLE_LENGTH: usize = 32;
+const LITERAL_CONTENT_LENGTH: usize = HANDLE_LENGTH - METADATA_LENGTH;
+const CANONICAL_HASH_LENGTH: usize = HANDLE_LENGTH - UINT64_LENGTH - METADATA_LENGTH;
+
+#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+pub(crate) struct Handle {
+    size: u64,
+    accessibility: Accessibility,
+    content: Content,
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+pub(crate) enum Content {
+    Other {
+        object_type: Object,
+        data: Nonliteral,
+    },
+    Literal([u8; LITERAL_CONTENT_LENGTH]),
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+pub(crate) enum Nonliteral {
+    Canonical([u8; CANONICAL_HASH_LENGTH]),
+    Local(u64),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+pub(crate) enum Accessibility {
+    Strict,
+    Shallow,
+    Lazy,
+}
+
+impl TryFrom<u8> for Accessibility {
+    type Error = anyhow::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            0 => Self::Strict,
+            1 => Self::Shallow,
+            2 => Self::Lazy,
+            _ => bail!("Invalid number for Accessibility"),
+        })
+    }
+}
+
+impl Into<u8> for Accessibility {
+    fn into(self) -> u8 {
+        match self {
+            Accessibility::Strict => 0,
+            Accessibility::Shallow => 1,
+            Accessibility::Lazy => 2,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+pub(crate) enum Object {
+    Blob,
+    Tree,
+    Thunk,
+    Tag,
+}
+
+impl TryFrom<u8> for Object {
+    type Error = anyhow::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            0 => Self::Tree,
+            1 => Self::Thunk,
+            2 => Self::Blob,
+            3 => Self::Tag,
+            _ => bail!("Invalid number for Object"),
+        })
+    }
+}
+
+impl Into<u8> for Object {
+    fn into(self) -> u8 {
+        match self {
+            Object::Tree => 0,
+            Object::Thunk => 1,
+            Object::Blob => 2,
+            Object::Tag => 3,
+        }
+    }
+}
+
+impl Handle {
+    /// Parses a handle in format [unsigned 64 bit number as hex]-[u64 as hex]-[u64 as hex]-[u64 as hex].
+    /// For example, d9-0-4-0 or 10-0-0-2400000000000000.
+    pub(crate) fn from_hex(input: &str) -> Result<Self> {
+        let handle_content = input
+            .split('-')
+            .map(|i| u64::from_str_radix(i, 16))
+            .collect::<Result<Vec<_>, _>>()
+            .context("Failed to parse handle from hex")?;
+        ensure!(
+            handle_content.len() == 4,
+            "Expected handle with exactly 4 parts when parsing from hex"
+        );
+        let handle_content: [u64; 4] = handle_content.try_into().unwrap();
+        let handle_content: [u8; HANDLE_LENGTH] = handle_content
+            .into_iter()
+            .map(|i| i.to_le_bytes().into_iter())
+            .flatten()
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+        // metadata is
+        // if handle is literal:
+        //     | strict/shallow/lazy (2 bits) | 1 (1 bit) | size of blob (5 bits)
+        // if not a literal:
+        //     | strict/shallow/lazy (2 bits) | 0 (1 bit) | 00 | canonical/local (1 bit) | Blob/Tree/Thunk/Tag (2 bits)
+        let metadata: u8 = handle_content[HANDLE_LENGTH - 1];
+        let is_literal = metadata & 0b10_0000 != 0;
+        let accessibility =
+            Accessibility::try_from(metadata >> 6).context("getting accessibility bits")?;
+        if is_literal {
+            // Handle structure
+            // data (8 bytes) | data (8 bytes) | data (8 bytes) | data (7 bytes) | metadata (1 byte)
+            let literal_size = metadata & 0b1_1111;
+            let mut content = [0u8; LITERAL_CONTENT_LENGTH];
+            content.copy_from_slice(&handle_content[..LITERAL_CONTENT_LENGTH]);
+            return Ok(Self {
+                size: literal_size.into(),
+                accessibility,
+                content: Content::Literal(content),
+            });
+        }
+        let is_canonical = metadata & 0b100 != 0;
+        let object_type = Object::try_from(metadata & 0b11).context("getting object bits")?;
+        let size = u64::from_le_bytes(
+            handle_content[UINT64_LENGTH * 2..UINT64_LENGTH * 3]
+                .try_into()
+                .unwrap(),
+        );
+        if is_canonical {
+            // Handle structure
+            // hash (8 bytes) | hash (8 bytes) | size (8 bytes) | hash (7 bytes) | metadata (1 byte)
+            let mut hash = [0u8; CANONICAL_HASH_LENGTH];
+            println!("{:?}", handle_content);
+            hash[..UINT64_LENGTH * 2].copy_from_slice(&handle_content[..UINT64_LENGTH * 2]);
+            hash[UINT64_LENGTH * 2..]
+                .copy_from_slice(&handle_content[UINT64_LENGTH * 3..HANDLE_LENGTH - 1]);
+            return Ok(Self {
+                size,
+                accessibility,
+                content: Content::Other {
+                    object_type,
+                    data: Nonliteral::Canonical(hash),
+                },
+            });
+        }
+
+        let local_id = u64::from_le_bytes(handle_content[..UINT64_LENGTH].try_into().unwrap());
+        Ok(Self {
+            size,
+            accessibility,
+            content: Content::Other {
+                object_type,
+                data: Nonliteral::Local(local_id),
+            },
+        })
+    }
+
+    /// Reconstructs the hex string version of a Handle
+    pub(crate) fn to_hex(&self) -> String {
+        self.to_buffer()
+            .chunks_exact(UINT64_LENGTH)
+            .map(|s: &[u8]| format!("{:x}", u64::from_le_bytes(s.try_into().unwrap())))
+            .collect::<Vec<_>>()
+            .join("-")
+    }
+
+    fn to_buffer(&self) -> [u8; HANDLE_LENGTH] {
+        let mut out_content = [0_u8; HANDLE_LENGTH];
+
+        out_content[HANDLE_LENGTH - 1] |= Into::<u8>::into(self.accessibility) << 6;
+
+        let data = match &self.content {
+            Content::Literal(data) => {
+                out_content[..LITERAL_CONTENT_LENGTH].copy_from_slice(data);
+                out_content[HANDLE_LENGTH - 1] |= 0b10_0000;
+                out_content[HANDLE_LENGTH - 1] |= (self.size & 0xFF) as u8;
+                return out_content;
+            }
+            Content::Other { object_type, data } => {
+                out_content[HANDLE_LENGTH - 1] |= Into::<u8>::into(*object_type);
+                data
+            }
+        };
+
+        out_content[UINT64_LENGTH * 2..UINT64_LENGTH * 3].copy_from_slice(&self.size.to_le_bytes());
+
+        match data {
+            Nonliteral::Canonical(hash) => {
+                out_content[HANDLE_LENGTH - 1] |= 0b100;
+                out_content[..UINT64_LENGTH * 2].copy_from_slice(&hash[..UINT64_LENGTH * 2]);
+                out_content[UINT64_LENGTH * 3..HANDLE_LENGTH - 1]
+                    .copy_from_slice(&hash[UINT64_LENGTH * 2..]);
+            }
+            Nonliteral::Local(id) => {
+                out_content[..UINT64_LENGTH].copy_from_slice(&id.to_le_bytes());
+            }
+        }
+
+        out_content
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn blob() {
+        // &strict Blob(10|0|0|2400000000000000)
+        let mut content = [0; LITERAL_CONTENT_LENGTH];
+        content[0] = 16;
+        let handle_string = "10-0-0-2400000000000000";
+        let handle = Handle {
+            // created as a u32
+            size: 4,
+            accessibility: Accessibility::Strict,
+            content: Content::Literal(content),
+        };
+        assert_eq!(Handle::from_hex(handle_string).unwrap(), handle);
+        assert_eq!(handle_string, handle.to_hex());
+    }
+
+    #[test]
+    fn thunk() {
+        // &strict Thunk(d9|0|4|100000000000000)
+        let handle_string = "d9-0-4-100000000000000";
+        let handle = Handle {
+            // 4 entries
+            size: 4,
+            accessibility: Accessibility::Strict,
+            content: Content::Other {
+                object_type: Object::Thunk,
+                data: Nonliteral::Local(217),
+            },
+        };
+        assert_eq!(Handle::from_hex(handle_string).unwrap(), handle);
+        assert_eq!(handle_string, handle.to_hex());
+    }
+
+    #[test]
+    fn tag() {
+        // &strict Tag(862fcba5ecaade2c|4b24159ac7c28a29|3|715eb1e41f37d42)
+        let handle_string = "862fcba5ecaade2c-4b24159ac7c28a29-3-715eb1e41f37d42";
+        let mut content = [0; CANONICAL_HASH_LENGTH];
+        content[..UINT64_LENGTH].copy_from_slice(
+            &u64::from_str_radix("862fcba5ecaade2c", 16)
+                .unwrap()
+                .to_le_bytes(),
+        );
+        content[UINT64_LENGTH..UINT64_LENGTH * 2].copy_from_slice(
+            &u64::from_str_radix("4b24159ac7c28a29", 16)
+                .unwrap()
+                .to_le_bytes(),
+        );
+        // zero out the last byte because that is metadata
+        // and is not part of the content hash
+        content[UINT64_LENGTH * 2..].copy_from_slice(
+            &u64::from_str_radix("0015eb1e41f37d42", 16)
+                .unwrap()
+                .to_le_bytes()[0..UINT64_LENGTH - 1],
+        );
+        let handle = Handle {
+            // all tags have 3 entries
+            size: 3,
+            accessibility: Accessibility::Strict,
+            content: Content::Other {
+                object_type: Object::Tag,
+                data: Nonliteral::Canonical(content),
+            },
+        };
+        assert_eq!(Handle::from_hex(handle_string).unwrap(), handle);
+        assert_eq!(handle_string, handle.to_hex());
+    }
+}
